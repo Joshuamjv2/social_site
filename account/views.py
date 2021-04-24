f883eced71747b5b09bdb9968b1508bbf4f2060e
Joshuamjv2 <joshuamjv22@gmail.com>
2021-04-24 20:20:39 +0300

    added edit account for users

--- account/views.py
+++ account/views.py
@@ -1,7 +1,8 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.contrib import messages
 from django.contrib.auth import authenticate, login
-from .forms import LoginForm, UserRegistrationForm
+from .forms import LoginForm, UserRegistrationForm, UserEditForm, ProfileEditForm
 from .models import Profile
 from django.contrib.auth.decorators import login_required
 
@@ -44,3 +45,23 @@ def register(request):
     else:
         user_form = UserRegistrationForm()
     return render(request, 'account/register.html', {'user_form':user_form})
+
+@login_required
+def edit(request):
+    if request.method == 'POST':
+        user_form = UserEditForm(instance=request.user, data = request.POST)
+        profile_form = ProfileEditForm(
+            instance = request.user.profile,
+            data = request.POST,
+            files = request.FILES
+        )
+        if user_form.is_valid and profile_form.is_valid:
+            user_form.save()
+            profile_form.save()
+            messages.success(request, 'Profile Updated successfully.')
+        else:
+            messages.error(request, 'Error updating your profile.')
+    else:
+        user_form = UserEditForm(instance=request.user)
+        profile_form = ProfileEditForm(instance = request.user.profile)
+    return render(request, 'account/edit.html', {'user_form':user_form, 'profile_form':profile_form})
