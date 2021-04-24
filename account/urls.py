406d3e884cb412d3f5fa893f7df753a492445067
Joshuamjv2 <joshuamjv22@gmail.com>
2021-04-24 03:25:06 +0300

    added users and user profiles

--- account/urls.py
+++ account/urls.py
@@ -4,7 +4,8 @@ from . import views
 
 urlpatterns = [
     path('', views.dashboard, name='dashboard'),
-    path('', include('django.contrib.auth.urls'),)
+    path('', include('django.contrib.auth.urls'),),
+    path('register', views.register, name='register')
     # path('login', auth_views.LoginView.as_view(), name='login'),
     # path('logout', auth_views.LogoutView.as_view(), name='logout'),
 
