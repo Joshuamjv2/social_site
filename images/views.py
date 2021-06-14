2b75c108453b648adee6e389db827cd48223de74
Joshuamjv2 <joshuamjv22@gmail.com>
2021-06-14 06:54:10 +0300

    fixes

--- images/views.py
+++ images/views.py
@@ -1,37 +1,52 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import get_object_or_404
-from .models import Image
 from django.contrib import messages
-from .forms import ImageCreateForm
+from django.shortcuts import get_object_or_404
 from django.http import JsonResponse
 from django.views.decorators.http import require_POST
+from django.http import HttpResponse
+from django.core.paginator import Paginator, EmptyPage, \
+                                  PageNotAnInteger
+from common.decorators import ajax_required
+from .forms import ImageCreateForm
+from .models import Image
 
-# Create your views here.
+
 @login_required
 def image_create(request):
     if request.method == 'POST':
-        # send form
+        # form is sent
         form = ImageCreateForm(data=request.POST)
         if form.is_valid():
-            # form.data is valid
-            cd = form.cleaned_data
+            # form data is valid
             new_item = form.save(commit=False)
-            # assign current user to the item(data)
+
+            # assign current user to the item
             new_item.user = request.user
             new_item.save()
-            messages.success(request, 'Image added successfully.')
-            # redirect to new created Item detail view
+            messages.success(request, 'Image added successfully')
+
+            # redirect to new created item detail view
             return redirect(new_item.get_absolute_url())
     else:
-        # build the form provided by bookmarklet via GET
+        # build form with data provided by the bookmarklet via GET
         form = ImageCreateForm(data=request.GET)
-    return render(request, 'images/image/create.html', {'section':'images', 'form':form})
+
+    return render(request,
+                  'images/image/create.html',
+                  {'section': 'images',
+                   'form': form})
+
 
 def image_detail(request, id, slug):
     image = get_object_or_404(Image, id=id, slug=slug)
-    return render(request, 'images/image/detail.html', {'section':'images', 'image':image})
+    return render(request,
+                  'images/image/detail.html',
+                  {'section': 'images',
+                   'image': image})
 
+
+@ajax_required
 @login_required
 @require_POST
 def image_like(request):
@@ -44,7 +59,33 @@ def image_like(request):
                 image.users_like.add(request.user)
             else:
                 image.users_like.remove(request.user)
-            return JsonResponse ({'status':'ok'})
+            return JsonResponse({'status':'ok'})
         except:
             pass
     return JsonResponse({'status':'error'})
+
+
+@login_required
+def image_list(request):
+    images = Image.objects.all()
+    paginator = Paginator(images, 6)
+    page = request.GET.get('page')
+    try:
+        images = paginator.page(page)
+    except PageNotAnInteger:
+        # If page is not an integer deliver the first page
+        images = paginator.page(1)
+    except EmptyPage:
+        if request.is_ajax():
+            # If the request is AJAX and the page is out of range
+            # return an empty page
+            return HttpResponse('')
+        # If page is out of range deliver last page of results
+        images = paginator.page(paginator.num_pages)
+    if request.is_ajax():
+        return render(request,
+                      'images/image/list_ajax.html',
+                      {'section': 'images', 'images': images})
+    return render(request,
+                  'images/image/list.html',
+                   {'section': 'images', 'images': images})
